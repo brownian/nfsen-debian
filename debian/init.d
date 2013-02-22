058637e0c027ccfbe4822ed43618be3cbb5d1d96
John Ferlito <johnf@inodes.org>
2013-02-22 18:34:56 +1100

    fixup

--- debian/init.d
+++ debian/init.d
@@ -2,7 +2,7 @@
 ### BEGIN INIT INFO
 # Provides:          nfsen
 # Required-Start:    $network $local_fs $remote_fs
-# Required-Stop:
+# Required-Stop:     $remote_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: nfsen
